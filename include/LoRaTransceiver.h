b0ea35878b1c1fefa82babfa11a90643791b7dc4
jaidenmartinsonhatt <jaidenmartinsonhatt@gmail.com>
2020-03-21 13:18:12 -0700

    first commit, initializing repo

---
+++ include/LoRaTransceiver.h
@@ -0,0 +1,140 @@
+/*
+ * RESET -> Orange -> D27
+ * NSS   -> Yellow -> D15
+ * SCK   -> Grey   -> D14
+ * MOSI  -> Green  -> D13
+ * MISO  -> Blue   -> D12
+ * DIO0  -> White  -> D26
+ */
+
+#include <SPI.h>
+#include <LoRa.h>
+
+//enum dataType { voltage, current, temperature, flow, count};
+
+struct packet
+{
+    char type; //'r': request packet, 'v': voltage, 'i': current, 't': temperature, 'f': flow, 'c': count.
+    int data;
+};
+
+class LoRaTransceiver
+{
+private:
+    SPIClass m_hspi;
+    const int mk_ss;
+    const int mk_rst;
+    const int mk_dio0;
+    int m_syncWord;
+public:
+    LoRaTransceiver(int, int, int, int);
+    void initialize();
+    void request(packet*, int);
+    void respond(packet*);
+    void send(packet);
+    packet receive();
+};
+
+LoRaTransceiver::LoRaTransceiver(int ss, int rst, int dio0, int syncWord = -1):
+ m_hspi(HSPI)
+,mk_ss(ss)
+,mk_rst(rst)
+,mk_dio0(dio0)
+,m_syncWord(syncWord) 
+{}
+
+void LoRaTransceiver::initialize()
+{   
+  //setup LoRa transceiver module
+  LoRa.setPins(mk_ss, mk_rst, mk_dio0);
+  
+  //replace the LoRa.begin(---E-) argument with your location's frequency 
+  //433E6 for Asia
+  //866E6 for Europe
+  //915E6 for North America
+  LoRa.setSPI(m_hspi);
+  while (!LoRa.begin(915E6)) {
+    Serial.println(".");
+    delay(500);
+  }
+  // Change sync word (0xF3) to match the receiver
+  // The sync word assures you don't get LoRa messages from other LoRa transceivers
+  // ranges from 0-0xFF
+  LoRa.setSyncWord(m_syncWord);
+  Serial.println("LoRa Initializing OK!");
+}
+
+void LoRaTransceiver::request(packet *packets, int syncWord = -1)
+{
+    if (syncWord != -1)
+    {
+        m_syncWord = syncWord;
+        LoRa.setSyncWord(m_syncWord);
+    }
+    else if (m_syncWord == -1)
+    {
+        Serial.print("Error: Sync Word was NEVER Specified!");
+    }
+
+    send({'r', 0});
+    //Receive
+    packets[0] = receive();
+}
+
+void LoRaTransceiver::respond(packet *packets)
+{
+    packet request;
+    request = receive();
+    if (request.type == 'r')
+    {
+        //send
+        send(packets[0]);
+        return;
+    }
+    else
+    {
+        Serial.print("Invalid Request!\n");
+        return;
+    }
+}
+
+void LoRaTransceiver::send(packet toSend)
+{
+    //Send LoRa packet to receiver
+    LoRa.beginPacket();
+    LoRa.print(toSend.type);
+    LoRa.print(toSend.data);
+    LoRa.endPacket();
+    Serial.print("Sent: "); Serial.print(toSend.type); Serial.println(toSend.data);
+}
+
+packet LoRaTransceiver::receive()
+{
+    String LoRaData;
+    bool isReceived = 0;
+    while (!isReceived)
+    {
+        // try to parse packet
+        int packetSize = LoRa.parsePacket();
+        if (packetSize) {
+        // received a packet
+        Serial.print("Received packet '");
+
+        // read packet
+        while (LoRa.available()) {
+            LoRaData = LoRa.readString();
+            Serial.print(LoRaData);
+        }
+
+        // print RSSI of packet
+        Serial.print("' with RSSI ");
+        Serial.println(LoRa.packetRssi());
+        isReceived = 1;
+        }
+    }
+    packet received;
+    received.type = LoRaData.charAt(0);
+    LoRaData.remove(0);
+    received.data = LoRaData.toInt();
+    return received;
+}
---
+++ include/farmSensor.h
@@ -0,0 +1,70 @@
+#pragma once
+#include <Arduino.h>
+
+enum sensorType { digital, analog, soft };
+
+//template <class T>
+class farmSensor 
+{
+private:
+    int m_pin;
+    String m_name;
+    String m_unit;
+    sensorType m_type;
+    int m_data;
+    int readRaw();
+public:
+    farmSensor(int pin, sensorType type, String name, String unit);
+    void initialize();
+    String read(int *p_data);
+};
+
+//template <class T>
+farmSensor::farmSensor(int pin, sensorType type, String name, String unit)
+{
+    m_pin = pin;
+    m_type = type;
+    m_name = name;
+    m_unit = unit;
+    m_data = -1;
+}
+
+
+//template <class T>
+void farmSensor::initialize()
+{
+    pinMode(m_pin, INPUT);
+}
+
+//template <class T>
+int farmSensor::readRaw()
+{   
+    switch (m_type)
+    {
+    case digital:
+        return digitalRead(m_pin);
+        break;
+    case analog:
+        return analogRead(m_pin);
+        break;
+    case soft:
+        return m_data++; 
+        break;
+    default:
+        return -1;
+        break;
+    }
+}
+
+String farmSensor::read(int *p_data)
+{
+    int raw = readRaw();
+    *p_data = raw;
+    return m_name + String(": ") + String(raw) + String(" (") + m_unit + String(")");
+}
+
+// Sensors to be used:
+//  Voltage (V)
+//  Current (A)
+//  Water Flow ()
+//  Temperature (C)
